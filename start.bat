d66b0d6d7914e71a966cbe0ee9fa1a6533cec856
f2ry <58121330+f2ry@users.noreply.github.com>
2025-04-04 00:49:09 +0700

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,45 +1,32 @@
 @echo off
-REM ==============================================
-REM Автозапуск симулятора сетей для Windows
-REM ==============================================
-
-REM Переключение на UTF-8 и настройка консоли
+:: Упрощённый и надёжный вариант
 chcp 65001 > nul
-set PYTHONIOENCODING=utf-8
-set LC_ALL=ru_RU.UTF-8
-
-REM Установка совместимого шрифта (если доступно)
-reg add "HKCU\Console\%CD%start.bat" /v "FaceName" /t REG_SZ /d "Lucida Console" /f > nul 2>&1
-reg add "HKCU\Console\%CD%start.bat" /v "CodePage" /t REG_DWORD /d 65001 /f > nul 2>&1
-
-SET PY_SCRIPT=main.py
-SET VENV_DIR=venv
-SET REQUIREMENTS=requirements.txt
+setlocal enabledelayedexpansion
 
 echo.
-echo [1/3] Активация виртуального окружения...
-if not exist "%VENV_DIR%\Scripts\activate.bat" (
-    echo Ошибка: Виртуальное окружение не найдено!
-    echo Создайте его командой: python -m venv venv
-    pause
-    exit /b 1
-)
-call "%VENV_DIR%\Scripts\activate.bat"
-
-echo.
-echo [2/3] Проверка зависимостей...
-"%VENV_DIR%\Scripts\python.exe" -m pip install --upgrade pip
-"%VENV_DIR%\Scripts\python.exe" -m pip install -r "%REQUIREMENTS%"
-if errorlevel 1 (
-    echo Ошибка при установке зависимостей!
+echo [1/3] Инициализация...
+python --version > nul 2>&1 && (
+    echo   УСПЕХ: Python установлен
+) || (
+    echo   ОШИБКА: Python не найден!
     pause
     exit /b 1
 )
 
 echo.
-echo [3/3] Запуск симулятора...
-echo ==============================================
-"%VENV_DIR%\Scripts\python.exe" "%PY_SCRIPT%"
-echo ==============================================
+echo [2/3] Активация окружения...
+if not exist "venv\Scripts\activate.bat" (
+    echo   Создаём виртуальное окружение...
+    python -m venv venv || (
+        echo   ОШИБКА: Не удалось создать VENV
+        pause
+        exit /b 1
+    )
+)
+call "venv\Scripts\activate.bat"
+
+echo.
+echo [3/3] Запуск программы...
+venv\Scripts\python.exe main.py
 
 pause
